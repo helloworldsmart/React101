dbf0d759d5196735d40c4e1f2cf3aafe02711c76
MichaelChen <helloworldsmart3@gmail.com>
2020-10-27 22:51:49 +0800

    Next: 4-3 Garbage collection mark-and-sweep

---
+++ javascript.info/Objects: the basics/4-3-GarbageCollection.js
@@ -0,0 +1,18 @@
+// 4-3 Garbage collection
+
+// Reachability
+// https://en.wikipedia.org/wiki/Garbage_collection_(computer_science)
+
+let user = {
+  name: "Michael"
+};
+
+let admin = user;
+
+user = null;
+
+function marry(man, woman) {
+	
+}
+
+// mark-and-sweep
