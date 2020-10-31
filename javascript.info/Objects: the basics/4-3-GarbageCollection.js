6f23efda10d66db0fd607f77e46b96b05db56425
MichaelChen <helloworldsmart3@gmail.com>
2020-10-31 21:04:50 +0800

    Next: 4-4 Object methods

--- javascript.info/Objects: the basics/4-3-GarbageCollection.js
+++ javascript.info/Objects: the basics/4-3-GarbageCollection.js
@@ -1,5 +1,7 @@
 // 4-3 Garbage collection
 
+const { delete } = require("request-promise-native");
+
 // Reachability
 // https://en.wikipedia.org/wiki/Garbage_collection_(computer_science)
 
@@ -12,7 +14,29 @@ let admin = user;
 user = null;
 
 function marry(man, woman) {
-	
+  woman.husband = man;
+  man.wife = woman;
+
+  return {
+    father: man,
+    mother: woman
+  }
 }
 
+let family = marry({
+  name: "John"
+}, {
+  name: "Ann"  
+});
+
+
+delete family.father;
+delete family.mother.husband;
+
+family = null;
+
 // mark-and-sweep
+
+// - Generational collection
+// - Inercemental collection
+// - Idle-time collection
