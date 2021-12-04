047d1f78e488afef08ab53d42c65647f9db63126
MichaelChen <helloworldsmart3@gmail.com>
2021-12-04 15:01:11 +0800

    React and TypeScript: Explicit Component Type Annotations

--- TypeScript/rts/src/props/Child.tsx
+++ TypeScript/rts/src/props/Child.tsx
@@ -5,3 +5,14 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>{color}</div>
 };
+
+
+// export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+//   return <div>{color}</div>
+// };
+
+export const ChildAsFC: React.FunctionComponent<ChildProps> = ({ color }) => {
+  return <div>{color}</div>
+};
+
+// ChildAsFC.prototype
