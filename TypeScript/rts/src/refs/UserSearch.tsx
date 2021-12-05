6a0cd552eb16d13e37ea9c03e83746d5d1d7c63e
MichaelChen <helloworldsmart3@gmail.com>
2021-12-05 21:02:12 +0800

    React and TypeScript: 20. More on Refs

--- TypeScript/rts/src/refs/UserSearch.tsx
+++ TypeScript/rts/src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const users = [
   { name: 'Sarah', age: 20 },
@@ -8,8 +8,16 @@ const users = [
 
 const UserSearch: React.FC = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  // const inputRef = useRef<any>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string, age: number } | undefined>();
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
+  }, []);
 
   const onClick = () => {
     const foundUser = users.find((user) => {
