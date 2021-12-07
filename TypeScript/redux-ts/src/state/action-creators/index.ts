72d05f4f06e94573c945e55b228ae220cd19a5f6
MichaelChen <helloworldsmart3@gmail.com>
2021-12-08 00:41:58 +0800

    Fixed

--- TypeScript/redux-ts/src/state/action-creators/index.ts
+++ TypeScript/redux-ts/src/state/action-creators/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Dispatch } from "react";
+import { Dispatch } from "redux";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
